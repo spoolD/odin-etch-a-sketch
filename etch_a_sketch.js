7ccb35d3d883448ab550d802bfd0614402f680df
Davy Sproule <dsproule@vt.edu>
2022-08-25 19:45:50 -0400

    Initial commit/Setup files and initial grid logic

---
+++ etch_a_sketch.js
@@ -0,0 +1,14 @@
+function grid(size){
+    const container = document.querySelector('.grid-container');
+    container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-columns: repeat(${size}, 1fr);`)
+    for (let i = 0; i < size; i++){
+        for (let j = 0; j < size; j++){
+            let newDiv = document.createElement('div');
+            newDiv.textContent = 'x';
+            container.appendChild(newDiv);
+        }
+            
+    }
+}
+
+grid(16)
