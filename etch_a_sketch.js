c10816d23df51b73a9d20f62e54e012be7f4b4a5
Davy Sproule <dsproule@vt.edu>
2022-08-29 18:59:51 -0400

    Fix grid-container so height/width is constant

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,6 +1,11 @@
 function grid(size){
+    // Get grid container
     const container = document.querySelector('.grid-container');
-    container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-columns: repeat(${size}, 1fr);`)
+    
+    // Set size and number of pixels using CSS Grid. The size of the container is held constant
+    container.setAttribute('style', `grid-template-rows: repeat(${size}, ${container.clientHeight/size}px); grid-template-columns: repeat(${size}, ${container.clientWidth/size}px);`)
+    
+    // Create a div for each pixel
     for (let i = 0; i < size; i++){
         for (let j = 0; j < size; j++){
             let newDiv = document.createElement('div');
